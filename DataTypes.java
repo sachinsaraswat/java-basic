b94193bfa97f0d7afbfd9a13f3f1d8d53a958d0a
Sachin Saraswat <sswt.git@gmail.com>
2021-12-27 18:44:55 -0500

    Primitive Data types

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+
+
+    // primitives 
+    byte agentAge = 15;
+    char agentGender='M';
+    short numberofMilesTravelled = 6574;
+    String agentName= "Alex Rider";
+    float agentHeight = 5.7f;
+
+    int numberOfCopieSold = 8234685;
+    long numOfWordsWritten = 47385748674967L;
+    double moneyEarnedByNovelSeries = 293474754.455;
+    boolean forTeens = true;
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Data Types.....");
+    }
+    
+}
